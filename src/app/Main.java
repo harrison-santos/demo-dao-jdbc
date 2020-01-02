da985da0502c1b61006016ad7ff2b14c04f6939d
Harrison Augusto Santos <harrisonspeed1997@gmail.com>
2020-01-02 17:11:42 -0300

    Adição da entidade Department

---
+++ src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department d = new Department(1, "Books");
+		System.out.println(d);
+
+	}
+
+}
