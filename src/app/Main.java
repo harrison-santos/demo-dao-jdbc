07af3b22234686e633f05d30202ac886291b69c0
Harrison Augusto Santos <harrisonspeed1997@gmail.com>
2020-01-03 10:53:54 -0300

    Método findById SellerDaoJDBC implementado

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,10 +11,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Department d = new Department(1, "Books");
-		System.out.println(d);
-		Seller s = new Seller(1, "Harrison", "harrison@hotmail.com", new Date(), 3000.0, d);
-		System.out.println(s);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
